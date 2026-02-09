fdc178a941732aee5e31936fdae563843b0c1fc6
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-09 13:06:14 +0100

    add exercise track

---
+++ Palindromo/main.js
@@ -0,0 +1,23 @@
+//TRACCIA
+
+/* 
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma 
+
+
+
+Consigli del giorno
+Scriviamo sempre in italiano i passaggi che vogliamo fare
+Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+Domande da  farsi quando si crea una funzione:
+Come dovrebbe chiamarsi?
+Ho bisogno di parametri?
+Devo restituire un valore?
+Se sì, di che tipo?
+
+*/
+
+
+//HUMAN CODE
---
+++ Pari e dispari/main.js
@@ -0,0 +1,30 @@
+//TRACCIA
+
+/*  
+
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+
+
+
+
+
+Consigli del giorno
+Scriviamo sempre in italiano i passaggi che vogliamo fare
+Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+Domande da  farsi quando si crea una funzione:
+Come dovrebbe chiamarsi?
+Ho bisogno di parametri?
+Devo restituire un valore?
+Se sì, di che tipo?
+
+*/
+
+
+
+//HUMAN CODE
